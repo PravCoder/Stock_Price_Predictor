11dee33045014f97e10809e4873617d68be05d2d
pravboi <pravachanpatra@gmail.com>
2024-08-09 02:54:39 -0500

    src

--- src/inference.py
+++ src/inference.py
@@ -4,10 +4,10 @@ load_dotenv()
 import hopsworks
 import pandas as pd
 import numpy as np
-import config as config # not doing src.config
+import src.config as config # not doing src.config
 from datetime import datetime, timedelta
 
-from data import (
+from src.data import (
     add_new_features,
     interpolate_backfill_frontfill,
     transform_ts_data_into_features_target
